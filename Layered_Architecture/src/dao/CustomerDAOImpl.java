b9a3232f4a0a0e41fa157fd142cfa7a191950af0
Akila-Piumal <akilapiumal16@gmail.com>
2022-05-05 13:38:53 +0530

    CustomerDAO Interface Created to invoke methods that only customerDAO wants and CustomerDAOImpl class implements it

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * @since : 0.1.0
  **/
 
-public class CustomerDAOImpl implements CrudDAO<CustomerDTO,String> {
+public class CustomerDAOImpl implements CrudDAO<CustomerDTO,String> , CustomerDAO{
 
     @Override
     public ArrayList<CustomerDTO> getAll() throws SQLException, ClassNotFoundException {
